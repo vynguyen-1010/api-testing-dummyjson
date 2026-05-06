b88e2b97f477d9e94dff0119b1a3bfe76af8ca49
vynguyen-1010 <66165751+vynguyen-1010@users.noreply.github.com>
2026-05-06 22:26:22 +0700

    Split assertion layer
    Create a new assertion layer

--- src/test/java/assertions/user/UserAssertions.java
+++ src/test/java/assertions/user/UserAssertions.java
@@ -1,5 +1,16 @@
 package assertions.user;
 
+import models.user.User;
+import org.testng.Assert;
+
 public class UserAssertions {
-    
+    public static void verifyUser(User user, int expectedId) {
+
+        Assert.assertNotNull(user, "User object is null");
+
+        Assert.assertEquals(user.getId(), expectedId, "User ID mismatch");
+
+        Assert.assertNotNull(user.getFirstName(), "First name is null");
+        Assert.assertNotNull(user.getEmail(), "Email is null");
+    }
 }
